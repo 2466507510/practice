ebf679910e236966d952ae88ff1493d85d1c26ff
李谨进 <17854258229@163.com>
2018-11-19 15:51:13 +0800

    1

--- src/anno/JDBCConfig.java
+++ src/anno/JDBCConfig.java
@@ -1,6 +1,5 @@
 package anno;
 
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
@@ -8,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD,ElementType.TYPE})
+@Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
